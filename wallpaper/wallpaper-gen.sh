b9877ae7a36df388121f6d93987c4bb037374799
Matej Kollar <208115@mail.muni.cz>
2019-07-06 15:47:06 +0100

    Let hacks infulence what parameters they get

--- wallpaper/wallpaper-gen.sh
+++ wallpaper/wallpaper-gen.sh
@@ -6,7 +6,14 @@ declare -r PROGDIR=$( dirname "$( readlink -f "${0}" )" )
 
 declare -r HACKS_DIR=${PROGDIR}/hacks
 
+declare -r IMAGES_DIR=${PROGDIR}/images
+
 TARGET=${HOME}/.generated-bg.png
+
+function die() {
+    echo "${@}" >& 2
+    exit 1
+}
 
 function resolutions() {
     xrandr | sed -n 's/\s\+\([0-9]\+\)x\([0-9]\+\)\s\+.*\*.*/\1 \2/p'
@@ -18,11 +25,45 @@ function get_resolution() {
     echo "${x}x${y}"
 }
 
+function get_single_image_path() {
+    find "${IMAGES_DIR}" -type f | shuf -n1 | xargs realpath
+}
+
+function run_hack() {
+    local -r hack=${1}; shift
+    if [[ "${1}" != "usage:" ]]; then
+        die "Hack does not seem to follow the protocol."
+    fi
+    shift 1
+
+    local -a HACK_ARGS=( )
+
+    while [[ "${#}" -ne 0 ]]; do
+        local current=${1}; shift
+        case "${current}" in
+            RESOLUTION)
+                HACK_ARGS+=( "$( get_resolution )" )
+                ;;
+            DESTINATION)
+                HACK_ARGS+=( "${TARGET}" )
+                ;;
+            IMAGE)
+                HACK_ARGS+=( "$( get_single_image_path )" )
+                ;;
+            *) die "Protocol error (unknown parameter): ${current}"
+                ;;
+        esac
+    done
+    "${hack}" "${HACK_ARGS[@]}"
+}
+
 main() {
-    resolution=$( get_resolution )
     mapfile -t -d $'\0' hacks < <( find "${HACKS_DIR}" -executable -type f -print0 )
+
     hack=${hacks[$(( RANDOM % ${#hacks[@]} ))]}
-    "${hack}" "${resolution}" "${TARGET}"
+    mapfile -t usage < <( ${hack} | tr -d '\n' | tr ' ' '\n' )
+    run_hack "${hack}" "${usage[@]}"
+
     feh --image-bg black --bg-center "${TARGET}"
 }
 
