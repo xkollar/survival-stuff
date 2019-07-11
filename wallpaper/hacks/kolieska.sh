626a498cb67e4a062dcb92e1ba837d84fb1d6e3e
Matej Kollar <208115@mail.muni.cz>
2019-07-11 08:28:24 +0100

    Fill instead of fit

--- wallpaper/hacks/kolieska.sh
+++ wallpaper/hacks/kolieska.sh
@@ -13,7 +13,10 @@ declare -r IMAGE=${1?Image}; shift
 declare -r DESTINATION=${1?Destination}; shift
 
 convert "${IMAGE}" \
-    -scale ">${RESOLUTION}" -scale 12.5% -scale 800% \
+    -scale ">${RESOLUTION}^" \
+    -gravity Center \
+    -extent "${RESOLUTION}" \
+    -scale 12.5% -scale 800% \
     \( -size 8x8 canvas:black +antialias -fill white -draw 'circle 3,3 0,3' -write mpr:tile +delete \) \
     \( +clone -fill mpr:tile  -draw 'color 0,0 reset' \) \
     -compose darken -composite \
