999e5b7b7db1178ba10f9b20fcdabc15078e1282
Matej Kollar <208115@mail.muni.cz>
2019-11-01 17:09:13 +0000

    More beauty inspired by travels…

---
+++ wallpaper/hacks/hexagons.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -eu
+
+
+if [[ "${#}" -eq 0 ]]; then
+    echo "usage: RESOLUTION IMAGE DESTINATION"
+    exit 0
+fi
+
+declare -r RESOLUTION=${1?Resolution}; shift
+declare -r IMAGE=${1?Image}; shift
+declare -r DESTINATION=${1?Destination}; shift
+
+if [[ "${#}" -gt 0 ]]; then
+    echo Unexpected arguments: "${@}"
+    exit 1
+fi
+
+convert "${IMAGE}" \
+    -scale ">${RESOLUTION}^" \
+    -scale 5.5% \
+    txt: \
+| sed '1d; s/:.* /,/;' \
+| sed 's/,/ /;s/,/ /' \
+| awk '{printf "%d,%d,%s\n", ($1*2+$2%2)*10, $2*2*10, $3}' \
+| convert "${IMAGE}" \
+    -scale ">${RESOLUTION}^" \
+    -sparse-color voronoi '@-' \
+    ${DESTINATION}
