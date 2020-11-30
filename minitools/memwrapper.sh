417fd93f19e587d779520b7829c9d40eb7b54532
Matej Kollar <208115@mail.muni.cz>
2020-11-30 22:48:59 +0000

    Firefox is more hungry :-/

--- minitools/memwrapper.sh
+++ minitools/memwrapper.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 readonly MAX_MEMORY_SIZE=$((1024*1024*8))
-readonly VIRTUAL_MEMORY=$((1024*1024*4))
+readonly VIRTUAL_MEMORY=$((1024*1024*8))
 
 ulimit -v "${VIRTUAL_MEMORY}" -m "${MAX_MEMORY_SIZE}"
 
